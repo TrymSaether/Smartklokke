8345fc9ad317e7d8d07fe9c1445d2a783ae32315
Doofsky <70568409+Doofsky@users.noreply.github.com>
2021-12-15 17:16:12 +0100

    Fiksa hele driten sånn at den funker

---
+++ lib/Stepcounter.h
@@ -0,0 +1,11 @@
+// #include <ICM_20948.h>
+//H Constants
+// byte buff[];
+static unsigned long stepSum;
+static int currentxx;
+static int currentyy;
+static int currentzz;
+char str[];
+int xx, yy, zz;
+int vector;
+void stepInit();
--- src/main.cpp
+++ src/main.cpp
@@ -1,143 +1,123 @@
-#include <Wire.h>
+// Bibliotek
+#include <Adafruit_ICM20X.h>
+#include <Adafruit_ICM20948.h>
 #include <Adafruit_Sensor.h>
-#include <Adafruit_BME280.h>
-#include "UbidotsEsp32Mqtt.h"
-#include "SSD1306Wire.h" // legacy include: `#include "SSD1306.h"`
+#include <Wire.h>
 
-SSD1306Wire  display(0x3c, 18, 19);  //18=SDK  19=SCK  As per labeling on ESP32 DevKit
-#define SEALEVELPRESSURE_HPA (1013.25)
+// ICM-20948
+Adafruit_ICM20948 icm;
+sensors_event_t acc;
+sensors_event_t temp;
+sensors_event_t mag;
+sensors_event_t gyro;
 
-char buffT[6];
-char buffA[6];
-char buffH[6];
-char buffP[6];
-Adafruit_BME280 bme;
+// - Deklarasjon -
+static unsigned long stepSum = 0;
+int deltaStepSum = 0;
 
-const char *UBIDOTS_TOKEN = "BBFF-OGwA21Z6uhoggc3wiUHnvhLUPbnRsh";  // Put here your Ubidots TOKEN
-const char *WIFI_SSID = "Eirik sin iPhone";      // Put here your Wi-Fi SSID
-const char *WIFI_PASS = "1234567890";      // Put here your Wi-Fi password
-const char *DEVICE_LABEL = "esp32";   // Replace with the device label to subscribe to
-const char *VARIABLE_Temperature = "temperatur"; // Replace with your variable label to subscribe to
-const char *VARIABLE_Humidity = "humidity"; // Replace with your variable label to subscribe to
-const char *VARIABLE_Altitude = "altitude"; // Replace with your variable label to subscribe to
-const char *VARIABLE_Pressure = "pressure"; // Replace with your variable label to subscribe to
+// Akselerasjon
+float x, y, z;
+static float currentx = 0;
+static float currenty = 0;
+static float currentz = 0;
+static float currentVectorMag = 0;
 
+const float xErr = 0.80; // Sensorkorigeringsverdier.
+const float yErr = 0.50;
+const float zErr = 10.22;
+const float threshold = 0.05; // Grense for stegakselerasjon
+float vectorMag;
 
-const int PUBLISH_FREQUENCY = 5000; // Update rate in milliseconds
+// Diverse
+char str[50];
+byte buff[10];
 
-unsigned long timer;
-unsigned long timer1;
-
-Ubidots ubidots(UBIDOTS_TOKEN);
-
-void callback(char *topic, byte *payload, unsigned int length)
+void printData()
 {
-  buffT = 0;
-  buffA = 0;
-  buffH = 0;
-  buffP = 0;
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.println("] ");
-  for (int i = 0; i < length; i++)
-  {
-    Serial.println((char)payload[i]);
-    if (topic = "temperatur"){
-      *buffT += payload[i];
-    }
-    if (topic = "humidity"){
-      *buffH += payload[i];
-    }
-    if (topic = "altitude"){
-      *buffA += payload[i];
-    }
-    if (topic = "pressure"){
-      *buffP += payload[i];
-    }
-  }
+  Serial.print("\t\tAccel X: ");
+  Serial.print(acc.acceleration.x);
+  Serial.print(" \tY: ");
+  Serial.print(acc.acceleration.y);
+  Serial.print(" \tZ: ");
+  Serial.print(acc.acceleration.z);
+  Serial.println(" m/s^2 ");
+
+  Serial.print("\t\tMag X: ");
+  Serial.print(mag.magnetic.x);
+  Serial.print(" \tY: ");
+  Serial.print(mag.magnetic.y);
+  Serial.print(" \tZ: ");
+  Serial.print(mag.magnetic.z);
+  Serial.println(" uT");
+
+  Serial.print("\t\tGyro X: ");
+  Serial.print(gyro.gyro.x);
+  Serial.print(" \tY: ");
+  Serial.print(gyro.gyro.y);
+  Serial.print(" \tZ: ");
+  Serial.print(gyro.gyro.z);
+  Serial.println(" radians/s ");
   Serial.println();
+
+  Serial.print("\t\tTemperature ");
+  Serial.print(temp.temperature);
+  Serial.println(" deg C");
+
+  Serial.println(stepSum);
 }
 
-void setup() {
+
+
+void stepInit()
+{
+  x = acc.acceleration.x - xErr;
+  y = acc.acceleration.y - yErr;
+  z = acc.acceleration.z - zErr;
+  vectorMag = sqrt(exp((x - currentx)) + exp((y - currenty)) + exp((z - currentz)));
+  if (vectorMag <= currentVectorMag + threshold)
+  {
+    sprintf(str, "%d", stepSum);
+  }
+  else if (vectorMag > currentVectorMag + threshold)
+  {
+    stepSum++;
+  }
+  currentx = x;
+  currenty = y;
+  currentz = z;
+  currentVectorMag = vectorMag;
+  sprintf(str, "%d", stepSum);
+}
+bool stepAssert() // Debugging throw error if wiring wrong
+{
+}
+void setup()
+{
   Serial.begin(115200);
-  ubidots.connectToWifi(WIFI_SSID, WIFI_PASS);
-  ubidots.setCallback(callback);
-  ubidots.setup();
-  ubidots.reconnect();
-  timer = millis();
-  bool status;
-  status = bme.begin(0x76);  
-  display.init();
-  display.flipScreenVertically();
-  display.setFont(ArialMT_Plain_10);
+  bool status = icm.begin_I2C(0x68);
+  if (!status)
+  {
+    Serial.println("Could not find a valid ICM sensor, check wiring!");
+    while (1)
+      ;
+  }
+  Serial.println("ICM20948 Found!");
+  Serial.println("OK");
+
+  icm.setGyroRange(ICM20948_GYRO_RANGE_2000_DPS);
+  icm.setGyroRateDivisor(255);
+  icm.setMagDataRate(AK09916_MAG_DATARATE_10_HZ);
+  icm.setAccelRange(ICM20948_ACCEL_RANGE_16_G);
+  icm.setAccelRateDivisor(4095);
 }
 
-void loop() { 
-  if (!ubidots.connected()){
-    ubidots.reconnect();
+void loop()
+{
+  icm.getEvent(&acc, &gyro, &temp, &mag);
+  stepInit();
+  
+  if(stepSum>deltaStepSum){
+    printData();
   }
-  publis();
-  screen();
-  ubidots.loop();
-}
-void publis(){
-  if (abs(millis() - timer) > PUBLISH_FREQUENCY){
-    float Temperature = bme.readTemperature();
-    float Pressure = (bme.readPressure() / 100.0F);
-    float Altitude = bme.readAltitude(SEALEVELPRESSURE_HPA);
-    float Humidity = bme.readHumidity();
-    if (!isnan(Temperature)) {
-      ubidots.add(VARIABLE_Temperature, Temperature); // Insert your variable Labels and the value to be sent
-    }
-    if (!isnan(Humidity)) {
-        ubidots.add(VARIABLE_Humidity, Humidity); // Insert your variable Labels and the value to be sent 
-    }
-    if (!isnan(Temperature)) {
-      ubidots.add(VARIABLE_Altitude, Altitude); // Insert your variable Labels and the value to be sent
-    }
-    if (!isnan(Humidity)) {
-        ubidots.add(VARIABLE_Pressure, Pressure); // Insert your variable Labels and the value to be sent 
-    }
-    ubidots.publish(DEVICE_LABEL);
-    timer = millis();
-  }
-}
-void screen() {
-  if (abs(millis() - timer1) > PUBLISH_FREQUENCY){
-  ubidots.subscribeLastValue(DEVICE_LABEL, VARIABLE_Temperature);
-  ubidots.subscribeLastValue(DEVICE_LABEL, VARIABLE_Humidity);
-  ubidots.subscribeLastValue(DEVICE_LABEL, VARIABLE_Altitude);
-  ubidots.subscribeLastValue(DEVICE_LABEL, VARIABLE_Pressure);
-  display.clear();
-    display.setTextAlignment(TEXT_ALIGN_LEFT);
-    display.setFont(ArialMT_Plain_10);
-    display.drawString(0, 0, "Sensor");
-      
-    display.setFont(ArialMT_Plain_10);
-    display.drawString(0, 20, "Temperature");
-    display.drawString(75, 20, String(buffT));
-
-    display.setFont(ArialMT_Plain_10);
-    display.drawString(100, 20, "*C");
-
-    display.drawString(0, 30, "Humidity");
-    display.setFont(ArialMT_Plain_10);
-    display.drawString(75, 30, String(buffH));
-    display.setFont(ArialMT_Plain_10);
-    display.drawString(100, 30, "%");
-
-    display.drawString(0, 40, "Altitude");
-    display.setFont(ArialMT_Plain_10);
-    display.drawString(75, 40, String(buffA));
-    display.setFont(ArialMT_Plain_10);
-    display.drawString(100, 40, "m");
-
-    display.drawString(0, 50, "Pressure");
-    display.setFont(ArialMT_Plain_10);
-    display.drawString(75, 50, String(buffP));
-    display.setFont(ArialMT_Plain_10);
-    display.drawString(100, 50, "hPa");
-  display.display();
-  timer1 = millis();
-  }
+  deltaStepSum = stepSum;
 }
